6679f898cc01eb3c02b95c21bb90b905376ae15f
solidanii <solianadaniel97@gmail.com>
2023-03-23 03:39:54 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _upper - check if a letter is upper
+ * @x: the number to be checked
+ * Return: always 0
+ */
+int _upper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
