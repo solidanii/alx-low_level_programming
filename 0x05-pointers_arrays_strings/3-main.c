56a1b230c9b66a411ceff0494387b8302d533fc6
solidanii <solianadaniel97@gmail.com>
2023-03-27 10:52:34 -0700

    function prints string

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = " i do not fear computers. ifear the lack of them -Isaac asimov";
+	_puts(str);
+	return (0);
+}
