949ade8f4653b61da286e39b9f6a219ed498dcbf
solidanii <solianadaniel97@gmail.com>
2023-03-27 11:00:46 -0700

    reverse of strings

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "i do not fear computers. i fear the lack of them - isaac asimov";
+	print_rev(str);
+	return (0);
+}
