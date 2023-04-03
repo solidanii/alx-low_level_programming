d15bbeeef5dd8598c43cbc153864db46bde4d26e
solidanii <solianadaniel97@gmail.com>
2023-04-03 10:14:50 -0700

    chess

--- 0x07-pointers_arrays_strings/8-main.c
+++ 0x07-pointers_arrays_strings/8-main.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int c3[3][3] = {{0, 1, 5}, {10, 11, 12}, {1000, 101, 102},};
 	int c5[5][5] = {{0, 1, 5, 12124, 1234}, {10, 11, 12, 123521, 12512}, {100, 12401, 10452, 11542545, 1214543435},};
-	print_diagsum((int *)c3, 3);
-	print_diagsum((int *)c5, 5);
+	print_diagsums((int *)c3, 3);
+	print_diagsums((int *)c5, 5);
 	return (0);
 }
