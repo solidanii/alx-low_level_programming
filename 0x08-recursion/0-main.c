781db143ba836b0770d6dec5c901cdcada1e908a
solidanii <solianadaniel97@gmail.com>
2023-04-05 00:23:17 -0700

    recusrion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: always 0
+ */
+int main(void)
+{
+	_puts_recursion("puts with recusion");
+	return (0);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to print
+ * Return: on sucess 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
