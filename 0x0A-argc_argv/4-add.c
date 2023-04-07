e0f73c063c4d3ed1805c89f9643ddadee997f598
solidanii <solianadaniel97@gmail.com>
2023-04-07 01:59:44 -0700

    4adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check string
+ * @str: array str
+ * Return: always 0
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+/**
+ * main - print the name of program
+ * @argc: count arguments
+ * @argv: arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
