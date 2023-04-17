c08fb80edb574edd2fab0731ebd3258843ff4430
solidanii <solianadaniel97@gmail.com>
2023-04-17 05:58:24 -0700

    new type dog

---
+++ 0x0E-structures_typedef/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * main - check the code
+ * Return: always 0
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "bob";
+	printf("my name is %s, and i am %.1f :) - woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
