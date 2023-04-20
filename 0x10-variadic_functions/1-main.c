26a3f0341a069910736ae08b593d94eaacc5df91
solidanii <solianadaniel97@gmail.com>
2023-04-20 13:14:40 -0700

    1fun print

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ * main - check code
+ * Return: always 0
+ */
+int main(void)
+{
+	print_numbers(", ", 4, 0, 98, -1024, 402);
+	return (0);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers
+ * @separator: string to be printed
+ * @n: number of integer
+ * @...a varible
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index;
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nums);
+}
